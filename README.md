c123972776969dbad2e2c9c26a0bbbcfbf75c13c
Shruti Sharma <72591283+shrutiisharma@users.noreply.github.com>
2021-05-19 20:55:56 +0530

    Shared Prefs Done

--- README.md
+++ README.md
@@ -1,25 +1,7 @@
-# # Text & Scrolling Views (1.3)
+# # Shared Preferences (9.1)
 
-Link to this Codelab is [here](https://developer.android.com/codelabs/android-training-text-and-scrolling-views?index=..%2F..%2Fandroid-training#0).
+Link to this Codelab is [here](https://developer.android.com/codelabs/android-training-shared-preferences?index=..%2F..%2Fandroid-training#3).
 
-This app displays a scrollable TextView (a magazine article).
 
-### Things I learnt:
 
-- How to use XML code to add multiple `TextView` elements.
-
-- How to use XML code to define a scrolling `View`.
-
-- How to display free-form text with some HTML formatting tags.
-
-- How to style the `TextView` background color and text color.
-
-- How to include a web link in the text.
-
-### Install the app with [this](https://github.com/shrutiisharma/Codelabs/releases/download/1.3/app-debug.apk) link.
-
-### Videos of the application:
-
-- [Scrolling View](https://user-images.githubusercontent.com/72591283/118230496-657c8a00-b4ab-11eb-9771-fc82032054e7.mp4)
-
-- [Opening Web Link](https://user-images.githubusercontent.com/72591283/118230489-61506c80-b4ab-11eb-847c-aac863816bc4.mp4)
+### Install the app with [this](https://github.com/shrutiisharma/Codelabs/releases/download/9.1/app-debug.apk) link.
--- app/src/main/java/com/streamliners/codelabs/MainActivity.java
+++ app/src/main/java/com/streamliners/codelabs/MainActivity.java
@@ -2,22 +2,111 @@ package com.streamliners.codelabs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
+
+import com.streamliners.codelabs.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    /*
-     * This app displays a scrollable TextView (a magazine article).
-     * All changes were made to the layout. No code changes.
-     * The code below is part of the Empty Activity template.
-    * */
+    //create object for view binding
+    ActivityMainBinding b;
+
+    private int backgroundClr;
+    private int count = 0;
+
+    SharedPreferences mPreferences;
+
+    private String sharedPrefFile =
+            "com.example.android.hellosharedprefs";
+
+    /**
+     * It initialises the activity.
+     * @param savedInstanceState : reference to a Bundle object that is passed into the onCreate method of every Android Activity
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        setupLayout();
+
+        // Initialize Shared Preferences
+        mPreferences = getSharedPreferences(sharedPrefFile, MODE_PRIVATE);
+        // Restore preferences
+        if (mPreferences != null) {
+            count = mPreferences.getInt(Constants.COUNT_KEY, 0);
+            b.output.setText(String.format("%s", count));
+            backgroundClr = mPreferences.getInt(Constants.COLOR_KEY, getResources().getColor(R.color.gray));
+            b.output.setBackgroundColor(backgroundClr);
+        }
     }
 
+    /**
+     * To set the layout of the sharedPrefsActivity
+     */
+    private void setupLayout() {
+        b = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(b.getRoot());
+
+        setTitle("Hello Shared Preferences");
+    }
+
+    public void setBlackClrBackground(View view) {
+        backgroundClr = getResources().getColor(R.color.black);
+        updateBackgroundColor();
+    }
+
+    public void setRedClrBackground(View view) {
+        backgroundClr = getResources().getColor(R.color.red);
+        updateBackgroundColor();
+    }
+
+    public void setBlueClrBackground(View view) {
+        backgroundClr = getResources().getColor(R.color.blue);
+        updateBackgroundColor();
+    }
+
+    public void setGreenClrBackground(View view) {
+        backgroundClr = getResources().getColor(R.color.green);
+        updateBackgroundColor();
+    }
+
+    private void updateBackgroundColor() {
+        b.output.setBackgroundColor(backgroundClr);
+    }
+
+    public void incCount(View view) {
+        count ++;
+        b.output.setText(String.valueOf(count));
+    }
+
+    public void resetOutput(View view) {
+
+        // Reset background color
+        backgroundClr = getResources().getColor(R.color.gray);
+        updateBackgroundColor();
+
+        // Reset count
+        count = 0;
+        b.output.setText(String.valueOf(count));
+
+        // Clear preferences
+        SharedPreferences.Editor preferencesEditor = mPreferences.edit();
+        preferencesEditor.clear();
+        preferencesEditor.apply();
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        mPreferences = getSharedPreferences(sharedPrefFile, MODE_PRIVATE);
+
+        SharedPreferences.Editor preferencesEditor = mPreferences.edit();
+        preferencesEditor.putInt(Constants.COUNT_KEY, count);
+        preferencesEditor.putInt(Constants.COLOR_KEY, backgroundClr);
+        preferencesEditor.apply();
+    }
 }
