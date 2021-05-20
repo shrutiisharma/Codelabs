d2450404bfd8167afaea030de514b0ba0ac4e12a
Shruti Sharma <72591283+shrutiisharma@users.noreply.github.com>
2021-05-20 08:41:46 +0530

    ConstraintLayout2 done!

--- README.md
+++ README.md
@@ -1,9 +1,21 @@
-# Constraint Layout 1
+# Constraint Layout 2
 
-Link to this Codelab is [here](https://developer.android.com/codelabs/constraint-layout#0).
+Link to this Codelab is [here](https://developer.android.com/codelabs/kotlin-android-training-constraint-layout#1).
 
 ### Install the app with [this](https://github.com/shrutiisharma/Codelabs/releases/download/constraintLayout1/app-debug.apk) link.
 
-#### Screenshot of the app:
+#### Screenshots of the app:
 
-<img title="" src="https://user-images.githubusercontent.com/72591283/118862473-65411c00-b8fb-11eb-9d14-cddf6d86e1ae.jpg" alt="constraint layout1" width="256">
+<img src="https://user-images.githubusercontent.com/72591283/118912930-2dab9180-b946-11eb-9eab-0fbc4d099bb2.jpg" title="" alt="Screenshot_2021-05-20-08-27-11-49_e7468760fead2db197506a0f3448409d" width="174">
+
+<img src="https://user-images.githubusercontent.com/72591283/118912950-3308dc00-b946-11eb-8c5c-756638e6d33f.jpg" title="" alt="Screenshot_2021-05-20-08-27-29-55_e7468760fead2db197506a0f3448409d" width="178">
+
+<img title="" src="https://user-images.githubusercontent.com/72591283/118912955-33a17280-b946-11eb-8d94-52a41d3fba9e.jpg" alt="Screenshot_2021-05-20-08-27-33-19_e7468760fead2db197506a0f3448409d" width="177">
+
+<img title="" src="https://user-images.githubusercontent.com/72591283/118912959-34d29f80-b946-11eb-9eb5-c25dc9f76ecb.jpg" alt="Screenshot_2021-05-20-08-27-37-19_e7468760fead2db197506a0f3448409d" width="174">
+
+<img title="" src="https://user-images.githubusercontent.com/72591283/118912961-356b3600-b946-11eb-918e-04946ebb65ec.jpg" alt="Screenshot_2021-05-20-08-26-51-76_e7468760fead2db197506a0f3448409d" width="176">
+
+<img src="https://user-images.githubusercontent.com/72591283/118912963-369c6300-b946-11eb-898b-9a9a17c25f78.jpg" title="" alt="Screenshot_2021-05-20-08-27-00-49_e7468760fead2db197506a0f3448409d" width="175">
+
+<img src="https://user-images.githubusercontent.com/72591283/118912966-3734f980-b946-11eb-8cc0-4e3a4a81f420.jpg" title="" alt="Screenshot_2021-05-20-08-27-06-76_e7468760fead2db197506a0f3448409d" width="176">
--- app/src/main/java/com/streamliners/codelabs/MainActivity.java
+++ app/src/main/java/com/streamliners/codelabs/MainActivity.java
@@ -1,18 +1,21 @@
 package com.streamliners.codelabs;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.streamliners.codelabs.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     //create object for view binding
     ActivityMainBinding b;
-
 
     /**
      * It initialises the activity.
@@ -32,7 +35,59 @@ public class MainActivity extends AppCompatActivity {
         b = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(b.getRoot());
 
-        setTitle("Layout CodeLab");
+        setTitle("Color My Views");
+        setupEventHandlers();
     }
 
+
+    /**
+     * To give color to the view accordingly
+     * @param view view to be colored
+     */
+    private void makeColored(View view) {
+        // checking the id of the view and setting the color to it accordingly
+        if (view.getId() == b.boxOneText.getId()) {
+            view.setBackgroundColor(Color.DKGRAY);
+        } else if (view.getId() == b.boxTwoText.getId()) {
+            view.setBackgroundColor(Color.GRAY);
+        } else if (view.getId() == b.boxThreeText.getId()) {
+            view.setBackgroundColor(Color.BLUE);
+        } else if (view.getId() == b.boxFourText.getId()) {
+            view.setBackgroundColor(Color.MAGENTA);
+        } else if (view.getId() == b.boxFiveText.getId()) {
+            view.setBackgroundColor(Color.BLUE);
+        } else if (view.getId() == b.redButton.getId()) {
+            b.boxThreeText.setBackgroundColor(getResources().getColor(R.color.red));
+        } else if (view.getId() == b.yellowButton.getId()) {
+            b.boxFourText.setBackgroundColor(getResources().getColor(R.color.yellow));
+        } else if (view.getId() == b.greenButton.getId()) {
+            b.boxFiveText.setBackgroundColor(getResources().getColor(R.color.green));
+        } else {
+            view.setBackgroundColor(Color.LTGRAY);
+        }
+    }
+
+
+    /**
+     *  Set event handlers(onClick) for views
+     */
+    private void setupEventHandlers() {
+        // list of all the clickable views
+        List<View> clickableViews = new ArrayList<>(
+                Arrays.asList(b.boxOneText, b.boxTwoText, b.boxThreeText,
+                        b.boxFourText, b.boxFiveText, b.constraintLayout,
+                        b.redButton, b.yellowButton, b.greenButton)
+        );
+
+        // setting the on click listeners to all the views using loop
+        for (View item :
+                clickableViews) {
+            item.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    makeColored(item);
+                }
+            });
+        }
+    }
 }
